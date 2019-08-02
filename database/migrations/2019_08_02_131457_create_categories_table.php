9672ad3899daf15a3a19f03ac02e3c6f2ae1122a
vagrant <vagrant@homestead>
2019-08-02 14:05:56 +0800

    类目 seeder 文件

--- database/migrations/2019_08_02_131457_create_categories_table.php
+++ database/migrations/2019_08_02_131457_create_categories_table.php
@@ -16,9 +16,9 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id')->comment('主键');
             $table->string('name')->comment('类目名称');
-            $table->unsignedBigInteger('parent_id')->comment('父类目 ID');
+            $table->unsignedBigInteger('parent_id')->nullable()->comment('父类目 ID');
             $table->foreign('parent_id')->references('id')->on('categories')->ondelete('cascade');
-            $table->unsignedTinyInteger('is_directory')->comment('是否拥有子类目');
+            $table->boolean('is_directory')->comment('是否拥有子类目');
             $table->unsignedInteger('level')->comment('当前类目层级');
             $table->string('path')->comment('该类目所有父类目id');
             $table->timestamps();
