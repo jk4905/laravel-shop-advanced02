49da1f0f7fc218650b8967368b986eea85e6d6ee
vagrant <vagrant@homestead>
2019-08-02 13:39:45 +0800

    初始化类目模型

---
+++ app/Models/Category.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'level',
+        'is_directory',
+        'path'
+    ];
+    protected $casts = [
+        'is_directory' => 'boolean',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听 Category 的创建事件，用于初始化 path 和 level 字段值
+        static::creating(function (Category $category) {
+//             如果创建的是一个根类目
+            if (is_null($category->parent_id)) {
+//                层级为0
+                $category->level = 0;
+//                祖宗为 -
+                $category->path = '-';
+            } else {
+                // 将层级设为父类目的层级 + 1
+                $category->level = $category->parent->level + 1;
+                // 将 path 值设为父类目的 path 追加父类目 ID 以及最后跟上一个 - 分隔符
+                $category->path = $category->parent->path . $category->parent->id . '-';
+            }
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    // 定一个一个访问器，获取所有祖先类目的 ID 值
+    public function getPathIdsAttribute()
+    {
+        return array_filter(explode('-', trim($this->path, '-')));
+    }
+
+    // 定义一个访问器，获取所有祖先类目并按层级排序
+    public function getAncestorsAttribute()
+    {
+        return Category::query()
+            // 使用上面的访问器获取所有祖先类目 ID
+            ->whereIn('id', $this->path_ids)
+            // 按层级排序
+            ->orderBy('level')->get();
+    }
+
+    // 定义一个访问器，获取以 - 为分隔的所有祖先类目名称以及当前类目的名称
+    public function getFullNameAttribute()
+    {
+        return $this->ancestors // 取出所有的祖先类目
+        ->pluck('name') // 将键名为 name 的取出作为一个新的数组
+        ->push($this->name) // 把当前类目名称放入数组最后一个
+        ->implode('-'); // 通过 - 分隔符连成字符串
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,16 +8,28 @@ use Illuminate\Support\Str;
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'title',
+        'description',
+        'image',
+        'on_sale',
+        'rating',
+        'sold_count',
+        'review_count',
+        'price'
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
     // 与商品SKU关联
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 
     public function getImageUrlAttribute()
