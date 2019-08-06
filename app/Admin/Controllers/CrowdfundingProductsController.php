f32ea3616bb7c588dd0880e2225b7c314ad773db
vagrant <vagrant@homestead>
2019-08-06 17:31:01 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -12,42 +12,13 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class CrowdfundingProductsController extends Controller
+class CrowdfundingProductsController extends CommonProductsController
 {
     use HasResourceActions;
 
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function index(Content $content)
+    public function getProductType()
     {
-        return $content->header('众筹商品列表')->body($this->grid());
-    }
-
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content->header('编辑众筹商品')->body($this->form()->edit($id));
-    }
-
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content->header('创建众筹商品')->body($this->form());
+        return Product::TYPE_CROWDFUNDING;
     }
 
     /**
@@ -55,12 +26,8 @@ class CrowdfundingProductsController extends Controller
      *
      * @return Grid
      */
-    protected function grid()
+    protected function customGrid(Grid $grid)
     {
-        $grid = new Grid(new Product);
-        // 使用 with 来预加载商品类目数据，减少 SQL 查询
-        $grid->model()->where('type', Product::TYPE_CROWDFUNDING)->with(['category']);
-
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
         // Laravel-Admin 支持用符号 . 来展示关联关系的字段
@@ -78,18 +45,6 @@ class CrowdfundingProductsController extends Controller
             return CrowdfundingProduct::$statusMap[$value];
         });
 
-        $grid->actions(function ($actions) {
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-        $grid->tools(function ($tools) {
-            // 禁用批量删除按钮
-            $tools->batch(function ($batch) {
-                $batch->disableDelete();
-            });
-        });
-
-        return $grid;
     }
 
 
@@ -98,50 +53,10 @@ class CrowdfundingProductsController extends Controller
      *
      * @return Form
      */
-    protected function form()
+    protected function customForm(Form $form)
     {
-        $form = new Form(new Product);
-
-        // 在表单中添加一个名为 type，值为 Product::TYPE_CROWDFUNDING 的隐藏字段
-        $form->hidden('type', '类型')->value(Product::TYPE_CROWDFUNDING);
-        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
-        $form->text('title', '商品名称')->rules('required');
-
-        // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式来搜索添加
-//        其中 ->options() 用于编辑商品时展示该商品的类目，Laravel-Admin 会把 category_id 字段值传给匿名函数，匿名函数需要返回 [id => value] 格式的返回值。
-        $form->select('category_id', '类目')->options(function ($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-
-        // 创建一个选择图片的框
-        $form->image('image', '封面图片')->rules('required|image');
-
-        // 创建一个富文本编辑器
-        $form->editor('description', '商品描述')->rules('required');
-
-        // 创建一组单选框
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-
         // 添加众筹相关字段
         $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.1');
         $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
-
-        // 直接添加一对多的关联模型
-        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-        });
-
-        // 定义事件回调，当模型即将保存时会触发这个回调
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-        });
-
-        return $form;
     }
 }
