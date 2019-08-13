09af7224adb5d5133adb2efcbdb29b8253efec9a
vagrant <vagrant@homestead>
2019-08-13 19:33:12 +0800

    减少秒杀接口 SQL 查询

--- app/Http/Requests/SeckillOrderRequest.php
+++ app/Http/Requests/SeckillOrderRequest.php
@@ -25,26 +25,30 @@ class SeckillOrderRequest extends FormRequest
             'address.zip'           => 'required',
             'address.contact_name'  => 'required',
             'address.contact_phone' => 'required',
-            'sku_id'     => [
+            'sku_id'                => [
                 'required',
                 function ($attribute, $value, $fail) {
-                    if (!$sku = ProductSku::find($value)) {
+                    // 从 Redis 中读取数据
+                    $stock = \Redis::get('seckill_sku_' . $value);
+                    if (is_null($stock)) {
                         return $fail('该商品不存在');
                     }
-                    if ($sku->product->type !== Product::TYPE_SECKILL) {
-                        return $fail('该商品不支持秒杀');
+                    if ($stock < 1) {
+                        return $fail('该商品已售完');
                     }
+
                     if ($sku->product->seckill->is_before_start) {
                         return $fail('秒杀尚未开始');
                     }
                     if ($sku->product->seckill->is_after_end) {
                         return $fail('秒杀已经结束');
                     }
-                    if (!$sku->product->on_sale) {
-                        return $fail('该商品未上架');
+
+                    if (!$user = \Auth::user()) {
+                        throw new AuthenticationException('请先登录');
                     }
-                    if ($sku->stock < 1) {
-                        return $fail('该商品已售完');
+                    if (!$user->email_verified_at) {
+                        throw new InvalidRequestException('请先验证邮箱');
                     }
                     if ($order = Order::query()
                         // 筛选出当前用户的订单
@@ -63,7 +67,8 @@ class SeckillOrderRequest extends FormRequest
 
                         return $fail('你已经下单了该商品，请到订单页面支付');
                     }
-                }],
+                }
+            ],
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -40,13 +40,13 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
-    Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 
     Route::get('installments', 'InstallmentsController@index')->name('installments.index');
     Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
     Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
     Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
 });
+Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
